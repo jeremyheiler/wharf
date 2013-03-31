f03d68095e2fbabbc2c04943ddf21c9a0546bd9e
Jeremy Heiler <jeremyheiler@gmail.com>
2013-03-31 16:27:16 -0400

    Bump Clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.blancas/kern "0.6.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.0"]]}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
